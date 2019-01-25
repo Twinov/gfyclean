8196537e94b08a2ef3e93054d24f6f9eaef1244d
Marc Hoeltge <hoeltgemarc@gmail.com>
2019-01-25 10:31:54 -0500

    Add files via upload

--- removeGfy.js
+++ removeGfy.js
@@ -1,4 +1,7 @@
 var vidTag = document.getElementsByTagName("video")[0];
 vidTag = vidTag.outerHTML;
 vidTag = vidTag.substring(0, 6) + " controls loop autoplay>" + vidTag.substring(vidTag.indexOf("<source"));
-document.body.innerHTML = vidTag;
+var divForCentering = "<div class=\"container\">" + vidTag + "</div>";
+document.body.innerHTML = divForCentering;
+document.head.innerHTML = "";
+//document.body.innerHTML = document.getElementsByTagName("video")[0].outerHTML.substring(0, 6) + " controls loop>" + document.getElementsByTagName("video")[0].outerHTML.substring(document.getElementsByTagName("video")[0].outerHTML.indexOf("<source"));
