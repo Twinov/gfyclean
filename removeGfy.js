d294468c0cd819f290f2be17b00cc4cf582f48a0
Marc Hoeltge <hoeltgemarc@gmail.com>
2019-01-27 17:24:32 -0500

    Memory efficiency improvements

--- removeGfy.js
+++ removeGfy.js
@@ -1,7 +1,10 @@
-var vidTag = document.getElementsByTagName("video")[0];
-vidTag = vidTag.outerHTML;
-vidTag = vidTag.substring(0, 6) + " controls loop autoplay>" + vidTag.substring(vidTag.indexOf("<source"));
-var divForCentering = "<div class=\"container\">" + vidTag + "</div>";
-document.body.innerHTML = divForCentering;
-document.head.innerHTML = "";
-//document.body.innerHTML = document.getElementsByTagName("video")[0].outerHTML.substring(0, 6) + " controls loop>" + document.getElementsByTagName("video")[0].outerHTML.substring(document.getElementsByTagName("video")[0].outerHTML.indexOf("<source"));
+//var vidTag = document.getElementsByTagName("video")[0];
+//vidTag = vidTag.outerHTML;
+//vidTag = vidTag.substring(0, 6) + " controls loop autoplay>" + vidTag.substring(vidTag.indexOf("<source"));
+//var divForCentering = "<div class=\"container\">" + vidTag + "</div>";
+//document.body.innerHTML = divForCentering;
+//document.head.innerHTML = "";
+
+//The below line does the above with a tiny bit of memory efficiency.
+
+document.body.innerHTML = "<div class=\"container\">" + document.getElementsByTagName("video")[0].outerHTML.substring(0, 6) + " controls loop autoplay>" + document.getElementsByTagName("video")[0].outerHTML.substring(document.getElementsByTagName("video")[0].outerHTML.indexOf("<source")) + "</div>";
