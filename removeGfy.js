80164b8443260905e66975fa382873cbfe858fb2
Twinov <hoeltgemarc@gmail.com>
2019-10-18 02:40:54 -0400

    rewrite to make it actually work with slow loading gfycat.

--- removeGfy.js
+++ removeGfy.js
@@ -1,10 +1,27 @@
-//var vidTag = document.getElementsByTagName("video")[0];
-//vidTag = vidTag.outerHTML;
-//vidTag = vidTag.substring(0, 6) + " controls loop muted autoplay>" + vidTag.substring(vidTag.indexOf("<source"));
-//var divForCentering = "<div class=\"container\">" + vidTag + "</div>";
-//document.body.innerHTML = divForCentering;
-//document.head.innerHTML = "";
+//new version of replacing the document body that should work
+//now uses window.onload to actually wait for the gfycat js to finish before obliterating the site.
+window.addEventListener ("load", gfyclean(), false)
 
-//The below line does the above with a tiny bit of memory efficiency.
-
-document.body.innerHTML = "<div class=\"container\">" + document.getElementsByTagName("video")[0].outerHTML.substring(0, 6) + " controls loop muted autoplay>" + document.getElementsByTagName("video")[0].outerHTML.substring(document.getElementsByTagName("video")[0].outerHTML.indexOf("<source")) + "</div>";
+function gfyclean() {
+    if (window.location.href != "https://gfycat.com/"){
+        try {
+            var vid = document.getElementsByClassName("video media")[0];
+            document.body.innerHTML = "<div class=\"container\"><video controls loop muted autoplay>" + vid.innerHTML + "</video></div>";
+        }
+        catch (TypeError) {
+            setTimeout(function() {
+                try {
+                    var vid = document.getElementsByClassName("video media")[0];
+                    document.body.innerHTML = "<div class=\"container\"><video controls loop muted autoplay>" + vid.innerHTML + "</video></div>";
+                }
+                catch (TypeError) {
+                    setTimeout(function() {
+                        var vid = document.getElementsByClassName("video media")[0];
+                        console.log(vid)
+                        document.body.innerHTML = "<div class=\"container\"><video controls loop muted autoplay>" + vid.innerHTML + "</video></div>";
+                    }, 6000);
+                }
+            }, 6000);
+        }
+    }
+};
